1896b0879f83f14682253d99b624fe94f08e89b0
HoshinoTouko <oranly@hotmail.com>
2017-09-11 23:34:02 +0800

    Add common function.

---
+++ src/main/java/moe/touko/SimpleChat/Common/Common.java
@@ -0,0 +1,56 @@
+package moe.touko.SimpleChat.Common;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Common {
+    public static String BeautifyDictionary(Map<String, Object> data){
+        ArrayList<String> resultList = new ArrayList<>();
+        String resultString;
+        for (Map.Entry<String, Object> entry: data.entrySet()){
+            String formatString = Common.isNumber(entry.getValue()) ? "%s" : "\"%s\"";
+            resultList.add(
+                    entry.getKey() + "=" + String.format(formatString, entry.getValue())
+            );
+        }
+        resultString = String.join(", ", resultList);
+        return resultString;
+    }
+    public static String BeautifyDictionary(Map<String, Object> data, ArrayList<String> keys){
+        ArrayList<String> resultList = new ArrayList<>();
+        String resultString;
+        for (Iterator<String> iter = keys.iterator(); iter.hasNext(); ){
+            String column = (String)iter.next();
+            try{
+                data.get(column);
+                if (Common.isNumber(data.get(column))) {
+                    resultList.add(String.format("%s", data.get(column)));
+                }
+                else{
+                    resultList.add(String.format("\"%s\"", data.get(column)));
+                }
+            }
+            catch (Exception e){
+                System.out.println(String.format("Error! Info is %s", e.getMessage()));
+                resultList.add("");
+            }
+        }
+        resultString = String.join(", ", resultList);
+        return resultString;
+    }
+    public static Boolean isNumber(Object object){
+        if (
+                object instanceof Short ||
+                        object instanceof Integer ||
+                        object instanceof Long ||
+                        object instanceof Float ||
+                        object instanceof Double
+                ){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
